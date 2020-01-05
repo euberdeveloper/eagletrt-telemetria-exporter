51ec3808b3d6dd4bfdee698c0ece76e976cbf692
euberdeveloper <euberdeveloper@gmail.com>
2020-01-05 22:43:30 +0100

    feat: zip name

--- vuejs/src/services/download.js
+++ vuejs/src/services/download.js
@@ -1,8 +1,20 @@
-export function downloadFile (data) {
+function getFileName() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth() + 1;
+    const day = now.getDate();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    return `${year}-${month}-${day}_${hours}_${minutes}_${seconds}.zip`;
+}
+
+export function downloadFile(data) {
     const url = window.URL.createObjectURL(new Blob([data]));
     const link = document.createElement('a');
     link.href = url;
-    link.setAttribute('download', 'zip.zip');
+    link.setAttribute('download', getFileName());
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
