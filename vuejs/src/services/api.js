ad7f6714c0b6b75b616be6b40f549feb2bb0fb88
euberdeveloper <euberdeveloper@gmail.com>
2020-01-06 21:45:05 +0100

    feat(frontend): ip problem
    config file for server ip address, overwritable by get url params

--- vuejs/src/services/api.js
+++ vuejs/src/services/api.js
@@ -1,20 +1,30 @@
 import axios from 'axios';
+import options from '../config.json';
 
-const HOST = 'localhost:8000';
+function getHostname() {
+    const url = new URL(window.location.href);
+    const hostname = url.searchParams.get('hostname');
+    const host = url.searchParams.get('host') || options.host;
+    const port = url.searchParams.get('port') || options.port;
+
+    return hostname || `${host}:${port}`;
+}
+
+const HOSTNAME = getHostname();
 
 export async function getDatabaseSchema () {
-    const response = await axios.get(`http://${HOST}/api/database-schema`);
+    const response = await axios.get(`http://${HOSTNAME}/api/database-schema`);
     return response.data;
 }
 
 export async function exportJson (selectedItems) {
     const body = { collectionsToExport: selectedItems };
-    const response = await axios.post(`http://${HOST}/api/export/json`, body, { responseType: 'blob' });
+    const response = await axios.post(`http://${HOSTNAME}/api/export/json`, body, { responseType: 'blob' });
     return response.data;
 }
 
 export async function exportCsv (selectedItems) {
     const body = { collectionsToExport: selectedItems };
-    const response = await axios.post(`http://${HOST}/api/export/csv`, body, { responseType: 'blob' });
+    const response = await axios.post(`http://${HOSTNAME}/api/export/csv`, body, { responseType: 'blob' });
     return response.data;
 }
