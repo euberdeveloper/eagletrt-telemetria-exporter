14ab6496492ce3047500b2d04c4fdc7a7c4adbdf
euberdeveloper <euberdeveloper@gmail.com>
2020-02-01 23:20:50 +0100

    chore: deployed

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: 8000,
     MONGO: {
-        uri: 'mongodb://localhost:27017',
+        uri: process.env.MONGO_URI || 'mongodb://localhost:27017',
         options: {}
     },
     LOGGER: {
