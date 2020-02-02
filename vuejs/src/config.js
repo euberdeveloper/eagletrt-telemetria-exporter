40fd20026ac6c22e142df187c7735558df21d062
euberdeveloper <euberdeveloper@gmail.com>
2020-02-02 01:56:51 +0100

    chore: normal conf

--- vuejs/src/config.js
+++ vuejs/src/config.js
@@ -1,11 +1,11 @@
-export default {
-    host: process.env.NODE_ENV === 'production' ? 'telemetria-exporter-demo.herokuapp.com' : 'localhost',
-    port: process.env.NODE_ENV === 'production' ? '' : 8000,
-    protocol: process.env.NODE_ENV === 'production' ? 'https' : 'http'
-};
-
 // export default {
-//     host: 'localhost',
-//     port: 8000,
-//     protocol: 'http'
+//     host: process.env.NODE_ENV === 'production' ? 'telemetria-exporter-demo.herokuapp.com' : 'localhost',
+//     port: process.env.NODE_ENV === 'production' ? '' : 8000,
+//     protocol: process.env.NODE_ENV === 'production' ? 'https' : 'http'
 // };
+
+export default {
+    host: 'localhost',
+    port: 8000,
+    protocol: 'http'
+};
