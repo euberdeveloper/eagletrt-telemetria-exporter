b237a711b43a6387a752aafa3c9263b91c27cf38
euberdeveloper <euberdeveloper@gmail.com>
2020-02-02 01:09:37 +0100

    improv: env

--- vuejs/src/services/api.js
+++ vuejs/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import options from '@/config';
+import options from '../config.json';
 
 function getHostname () {
     const url = new URL(window.location.href);
@@ -11,21 +11,35 @@ function getHostname () {
 }
 
 const HOSTNAME = getHostname();
+<<<<<<< HEAD
 const PROTOCOL = options.protocol;
 
 export async function getDatabaseSchema () {
     const response = await axios.get(`${PROTOCOL}://${HOSTNAME}/api/database-schema`);
+=======
+
+export async function getDatabaseSchema () {
+    const response = await axios.get(`http://${HOSTNAME}/api/database-schema`);
+>>>>>>> parent of d6274cc... feat: prod
     return response.data;
 }
 
 export async function exportJson (selectedItems) {
     const body = { collectionsToExport: selectedItems };
+<<<<<<< HEAD
     const response = await axios.post(`${PROTOCOL}://${HOSTNAME}/api/export/json`, body, { responseType: 'blob' });
+=======
+    const response = await axios.post(`http://${HOSTNAME}/api/export/json`, body, { responseType: 'blob' });
+>>>>>>> parent of d6274cc... feat: prod
     return response.data;
 }
 
 export async function exportCsv (selectedItems) {
     const body = { collectionsToExport: selectedItems };
+<<<<<<< HEAD
     const response = await axios.post(`${PROTOCOL}://${HOSTNAME}/api/export/csv`, body, { responseType: 'blob' });
+=======
+    const response = await axios.post(`http://${HOSTNAME}/api/export/csv`, body, { responseType: 'blob' });
+>>>>>>> parent of d6274cc... feat: prod
     return response.data;
 }
